d1dc4e53ecf25807b67ab20f4e74dd612cc91201
Codekul-Ritesh <ritesh.tagunde2402@gmail.com>
2024-06-29 14:43:09 +0530

    registration design

--- client/src/OPD/PATIENT/AppointmentScheduling.js
+++ client/src/OPD/PATIENT/AppointmentScheduling.js
@@ -3,7 +3,10 @@ import React from 'react'
 function AppointmentScheduling() {
   return (
     <div>
-        <h1>AppointmentScheduling</h1></div>
+      <h1>
+        AppointmentScheduling page
+      </h1>
+    </div>
   )
 }
 
